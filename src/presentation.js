fc2cf0b0da594f8a0b4be07e889b090088b23e2e
shy_azusa <shy.azusa@gmail.com>
2019-03-11 21:52:21 +0900

    Add ReactCompareImage

--- src/presentation.js
+++ src/presentation.js
@@ -1,17 +1,17 @@
 // Import React
 import React from 'react';
+import ReactCompareImage from 'react-compare-image';
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
   ListItem,
   List,
-  Quote,
+  Link,
   Slide,
   Text,
+  GoToAction,
 } from 'spectacle';
 
 // Import theme
@@ -34,60 +34,105 @@ const theme = createTheme(
 );
 
 export default class Presentation extends React.Component {
+
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={['slide']}
         transitionDuration={500}
         theme={theme}
+        progress="bar"
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+        <Slide>
+          <Heading margin="0 0 50px" size={1} textColor="black">
+            TTNの画像diffスライド
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin="0 0 50px" size={1}>
+            竹井 彰平
           </Text>
+          <GoToAction slide={2}>Start</GoToAction>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
+        <Slide>
+          <Heading size={3}>
+            これはテスト用のスライドです
           </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Test1</ListItem>
+            <ListItem>Test2</ListItem>
+            <ListItem>Test3</ListItem>
+            <ListItem>Test4</ListItem>
+            <ListItem>Test5</ListItem>
+            <ListItem>Test6</ListItem>
           </List>
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+        <Slide>
+          <Heading size={5}>
+            まだテストなんだよなぁ
+          </Heading>
+          <List>
+            <ListItem>早く</ListItem>
+            <ListItem>diff</ListItem>
+            <ListItem>を</ListItem>
+            <ListItem>見たい</ListItem>
+            <ListItem>!!!</ListItem>
+          </List>
+        </Slide>
+        <Slide>
+          <GoToAction
+            render={goToSlide => (
+              <div style={{
+                maxHeight: 'auto',
+                maxWidth: 'auto',
+                margin: 'auto',
+              }}>
+                <ReactCompareImage
+                  leftImage="./img/1.jpg"
+                  rightImage="./img/2.jpg"
+                  sliderLineColor="red"
+                />
+              </div>
+            )}
+          />
+        </Slide>
+        <Slide>
+          <GoToAction
+            render={goToSlide => (
+              <div style={{
+                maxHeight: 'auto',
+                maxWidth: 'auto',
+                margin: 'auto',
+              }}>
+                <ReactCompareImage
+                  leftImage="./img/3.jpg"
+                  rightImage="./img/4.jpg"
+                  sliderLineColor="red"
+                />
+              </div>
+            )}
+          />
+        </Slide>
+        <Slide>
+          <GoToAction
+            render={goToSlide => (
+              <div style={{
+                maxHeight: 'auto',
+                maxWidth: 'auto',
+                margin: 'auto',
+              }}>
+                <ReactCompareImage
+                  leftImage="./img/5.jpg"
+                  rightImage="./img/6.jpg"
+                  sliderLineColor="red"
+                />
+              </div>
+            )}
+          />
+        </Slide>
+        <Slide>
+          <Heading size={5}>
+            おわり
+          </Heading>
+          <GoToAction slide={1}>最初に戻る</GoToAction>
         </Slide>
       </Deck>
     );
